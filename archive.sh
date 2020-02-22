cf181dd4626db0202ed0cd42f3ae7e32de3c616c
Vadim Kantorov <vadimkantorov@gmail.com>
2020-02-22 00:24:06 +0000

    ...

--- archive.sh
+++ archive.sh
@@ -2,7 +2,8 @@ set -e
 
 PROG=$1
 
-wget --quiet --recursive --no-clobber --no-parent -I /programs/$PROG/archive/ echo.msk.ru/programs/$PROG/index.html
+wget --no-verbose --no-clobber --no-parent --recursive --level inf -I /programs/$PROG/archive/ echo.msk.ru/programs/$PROG/index.html
+
 for p in echo.msk.ru/programs/$PROG/index.html echo.msk.ru/programs/$PROG/archive/*/index.html; do
 	python3 echomsk.py "$p" --archive --min-date $MINDATE --max-date $MAXDATE
 done | sort | uniq
