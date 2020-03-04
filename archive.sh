c4e2a51456dea6825b05cf6c0c3f97627f17bc21
Vadim Kantorov <vadimkantorov@gmail.com>
2020-03-04 16:02:46 +0100

    Delete archive.sh

--- archive.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-PROG=$1
-
-wget --no-verbose --no-clobber --no-parent --recursive --level inf -I /programs/$PROG/archive/ echo.msk.ru/programs/$PROG/index.html
-
-for p in echo.msk.ru/programs/$PROG/index.html echo.msk.ru/programs/$PROG/archive/*/index.html; do
-	python3 echomsk.py "$p" --archive --min-date $MINDATE --max-date $MAXDATE
-done | sort | uniq
