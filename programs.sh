e8e2bbaefb1fdc70c88a9387a02546cc808781bf
Vadim Kantorov <vadimkantorov@gmail.com>
2020-03-04 16:03:08 +0100

    Delete programs.sh

--- programs.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-wget --quiet --html-extension --force-directories http://echo.msk.ru/programs http://echo.msk.ru/programs/archived
-
-printf "Current shows:\n\n"
-python3 echomsk.py echo.msk.ru/programs.html --programs
-
-printf "\nArchived shows:\n\n"
-python3 echomsk.py echo.msk.ru/programs/archived.html --programs
